d19417ee0f352bde234dba01c8f380ae8bbfd124
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2021-02-02 14:25:45 +0100

    First commit

---
+++ docs/precache-manifest.489b6da32d655ac4c5596e83be9c4482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a709441563f8f2702dd2",
+    "url": "/vue-app-2/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f305c1e209c674bf7208de3becdf9eb4",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "1a96fce5b1b624bf999d",
+    "url": "/vue-app-2/js/about.0d0ec44c.js"
+  },
+  {
+    "revision": "a709441563f8f2702dd2",
+    "url": "/vue-app-2/js/app.d5d3b3b7.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
